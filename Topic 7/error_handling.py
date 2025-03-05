875ac7b1ef2a1c4fdceb667593047487e863aa07
Alan MacDonald <me@alanmacdonald.com>
2025-03-05 16:03:43 +0000

    Completed error_handling.py

--- Topic 7/error_handling.py
+++ Topic 7/error_handling.py
@@ -7,16 +7,56 @@ Date: 05/03/2025
 
 This program demonstrates the use of error handling in Python3.
 """
+# ============================================ #
+# //             Module imports             // #
+# ============================================ #
 
+import sys
+
+# ============================================ #
+# //      Global variable declarations      // #
+# ============================================ #
+
+
+# List of ints and strings
 my_List = ('1', 2, 3, '4', 'A', 5, 6, '7')
 
-# create a loop that prints the type and value of each element in my_list
-# the print() statement should use end = to prevent carriage return
-for element in my_List:
-    print(f"{type(element)} {element}", end=" ")
+# -------------------------------------------- #
+# //               Main function            // #
+# -------------------------------------------- #
 
-    try:
+
+def main():
+    # Loop with error checking for faults converting to int
+    for element in my_List:
+        print(f"{type(element)} {element} now ", end=" ")
+
+        try:
+            attempt_int = int(element)
+            print(f"{type(attempt_int)} {attempt_int}")
+        except ValueError:
+            print(f"{attempt_int} cannot be converted to an integer")
+
+    # Loop with no error checking for faults converting to int
+    for element in my_List:
+        print(f"{type(element)} {element} now ", end=" ")
+
         attempt_int = int(element)
-        print(f" now {type(attempt_int)} {attempt_int}")
-    except ValueError:
-        print(f" now ")
+        print(f"{type(attempt_int)} {attempt_int}")
+# END function main()
+
+# ========================================== #
+# //          Global environment          // #
+# ========================================== #
+
+
+# Call main function
+if __name__ == "__main__":
+    main()
+else:
+    sys.exit(1)
+
+# Exit program
+sys.exit(0)
+
+# END
