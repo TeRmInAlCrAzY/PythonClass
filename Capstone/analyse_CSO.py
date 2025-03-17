cd0a412df82a708788ca520136259c13897e272c
Alan MacDonald <me@alanmacdonald.com>
2025-03-17 20:47:40 +0000

    Added some more logic to start building output. Also built a list of lookups for town ids

--- Capstone/analyse_CSO.py
+++ Capstone/analyse_CSO.py
@@ -22,6 +22,26 @@ https://stackoverflow.com/questions/12965203/how-to-get-json-from-webpage-into-p
 from tools import *
 import urllib.request
 import json
+import sys
+import logging
+
+# ============================================ #
+# //      Global variable declarations      // #
+# ============================================ #
+
+
+LETTERG_CSV = "letterG.csv"
+LETTERK_CSV = "letterK.csv"
+CSV_HEADER = ["town_code",
+              "town_name",
+              "pop_value",
+              "males_value",
+              "females_value",
+              "ph_occ_value",
+              "ph_unocc_value",
+              "vac_dwell_value",
+              "h_stock_value",
+              "vac_rate_value",]
 
 # ============================================ #
 # //           getCounty Function           // #
@@ -45,54 +65,127 @@ def getCounty(town: str):
     """
     posCounty: int = town.find("Co.")
     county = town[posCounty+4:]
-    print(f"Position of Co. in {town} is {posCounty}, county is {county}")
+    logging.info(f"Position of Co. in {town} is {posCounty}, county is {county}")
     if county.find(",") != -1:
-        print("There is a comma in the county name")
+        logging.info("There is a comma in the county name")
         posComma = county.find(",")
-        print(f"The comma position is {posComma}")
-        print(f"The corrected county name is {county[:posComma]}")
+        logging.info(f"The comma position is {posComma}")
+        logging.info(f"The corrected county name is {county[:posComma]}")
         county=county[:posComma]
 
     return county
 
+def getTown(town: str):
+    posCounty: int = town.find("Co.")
+    town = town[3:posCounty-1]
+    return town
 
-with urllib.request.urlopen("https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/CD176/JSON-stat/1.0/en") as url:
-    data = json.load(url)
-    print(data)
+def main():
+    """This is the main() function"""
+    # Set the logging level to info
+    logging.basicConfig(level=logging.INFO)
 
-# save the data to a json file in the current working directory called CSO.json
-with open("CSO.json", "w") as outfile:
+    # Pull the JSON data from the suppied url
+    with urllib.request.urlopen("https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/CD176/JSON-stat/1.0/en") as url:
+        data = json.load(url)
+        logging.info(data)
+
+    # Save the data to a json file in the current working directory called CSO.json
+    outfile = open("CSO.json", "w")
     json.dump(data, outfile)
-
-# import CSO.json to a dictionary
-with open("CSO.json", "r") as infile:
+    outfile.close()
+    
+    # Import CSO.json to a dictionary
+    infile = open("CSO.json", "r")
     data = json.load(infile)
+    infile.close()
 
-#print the type of data
+    # Build a lookup list from $.dataset.dimension.C03198V03862.category.label
+    # This will allow us to look up the population data later
+    lookup = {}
+    for key, value in data["dataset"]["dimension"]["C03198V03862"]["category"]["label"].items():
+        # Set the value to be the first 3 characters of the value item
+        value = value[0:3]
+        lookup[value] = key
 
-printHeader("Type of Data")
-print(type(data))
+    logging.info("Lookup List")
+    logging.info(lookup)
 
-# Output the data relating to all towns starting with the letter 'G'
-printHeader("Towns starting with G")
-g_towns = data["dataset"]["dimension"]["C03198V03862"]["category"]["label"]
-print(g_towns)
+    # Extract a list of all values from $.dataset.value
+    values = data["dataset"]["value"]
+    logging.info("List of all values")
+    logging.info(values)
+    logging.info(type(values))
 
-# Declare a blank list of G towns
-g_towns_list = []
+    # make a list of all the towns
+    allTowns = {}
 
-printHeader("G towns noted")
-for town in (g_towns.values()):
-    print(town)
-    if town[4] == "G":
-        print("G Town!")
-        g_towns_list.append(town)
+    logging.info("Building list of all towns")
 
-printHeader("G towns list")
-for town in g_towns_list:
-    print(town)
-    county = getCounty(town)
-    print (county)
+    allTowns = data["dataset"]["dimension"]["C03198V03862"]["category"]["label"]
+    logging.info("***List of all towns")
+    logging.info(type(allTowns))
+    logging.info(allTowns)
+
+    # Declare a dictionary called gTowns
+    gTowns = {}
+    logging.info("Building dictionary of G towns")
+
+    # step through the list of towns in allTowns. print each key and value in allTowns
+    for key, town in allTowns.items():
+        print(f"Town: {town}")
+        if town[4] == "G":
+            # add town to dictionary gTowns with key index
+            gTowns[key] = town
+
+    logging.info("***Dictionary of G towns")
+    logging.info(gTowns)
+
+    # So we have our list of G towns in a dictionary. We now need to step through the items in the dictionary, extract the index, use
+    # that index to lookup the data in the values list
+
+    # Open the letterG.csv file
+    try:
+        fh = open(LETTERG_CSV, mode="w", encoding="utf8")
+    except PermissionError as e:
+        print(f"ERROR: Permission denied for file '{LETTERG_CSV}'.")
+        print(f"Logging: {e}")
+        sys.exit(1)
+    except Exception as e:
+        print(f"ERROR: New un-excepted error.h")
+        raise OSError(e)
+
+    logging.info(CSV_HEADER)
+
+
+    # Step through the gtowns
+    for key, town in gTowns.items()
+
+
+    #print the type of data
+
+    # logging.info(type(data))
+
+    # # Output the data relating to all towns starting with the letter 'G'
+    # g_towns_list = [town for town in allTowns.values() if town[4] == 'G']
+    # logging.info("Towns starting with G:")
+    # logging.info(g_towns_list)
+    #
+    # # Declare a blank list of G towns
+    # g_towns_list = []
+    #
+    # logging.info("G towns noted")
+    # for town in (g_towns.values()):
+    #     logging.info(town)
+    #     if town[4] == "G":
+    #         logging.info("G Town!")
+    #         g_towns_list.append(town)
+    #
+    # logging.info("G towns list")
+    # for town in g_towns_list:
+    #     logging.info(town)
+    #     county = getCounty(town)
+    #     logging.info(county)
 
 
 # # For each town, find the 4th character in the string
@@ -100,3 +193,20 @@ for town in g_towns_list:
 # for town in (g_towns.values()):
 #     print(town[3])
 
+# END function main()
+
+# ========================================== #
+# //          Global environment          // #
+# ========================================== #
+
+
+# Call main function
+if __name__ == "__main__":
+    main()
+else:
+    sys.exit(0)
+
+# Exit program
+sys.exit(0)
+
+# END
