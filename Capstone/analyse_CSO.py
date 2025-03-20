9d47de0cf71f2e8c3a1ae81bffec223affb12760
Alan MacDonald <me@alanmacdonald.com>
2025-03-20 17:08:34 +0000

    Linted as far as possible

--- Capstone/analyse_CSO.py
+++ Capstone/analyse_CSO.py
@@ -33,53 +33,63 @@ import logging
 
 LETTERG_CSV = "letterG.csv"
 LETTERK_CSV = "letterK.csv"
-CSV_HEADER = ["town_code",
-              "town_name",
-              "pop_value",
-              "males_value",
-              "females_value",
-              "ph_occ_value",
-              "ph_unocc_value",
-              "vac_dwell_value",
-              "h_stock_value",
-              "vac_rate_value",]
+CSV_HEADER = [
+    "town_code",
+    "town_name",
+    "pop_value",
+    "males_value",
+    "females_value",
+    "ph_occ_value",
+    "ph_unocc_value",
+    "vac_dwell_value",
+    "h_stock_value",
+    "vac_rate_value",
+]
 
 # ============================================ #
 # //           getCounty Function           // #
 # ============================================ #
 
+
 def getCounty(town: str):
     """
-    Extracts the county name from a given town string, specifically by identifying
-    the occurrence of "Co." within the string, followed by processing subsequent
-    characters to isolate and return the county name. If there is a comma in the
-    county name, it truncates the name up to the first comma to correct the
-    format.
-
-    :param town: A string representing the town name, including the "Co."
-        designation and potentially a trailing comma-separated county name.
-    :type town: str
-
-    :return: The extracted county name, trimmed and corrected of any
-        extraneous trailing commas or additional text.
-    :rtype: str
+    Given a string of Town data, return the county name
+    Also check to see if there is a comma in the county name,
+    and discard from there to the end of the string
     """
     posCounty: int = town.find("Co.")
-    county = town[posCounty+4:]
-    logging.info(f"Position of Co. in {town} is {posCounty}, county is {county}")
+    county = town[posCounty + 4:]
+    logging.info(f"Position of Co. in {town} is {posCounty},"
+                 f" county is {county}")
     if county.find(",") != -1:
         logging.info("There is a comma in the county name")
         posComma = county.find(",")
         logging.info(f"The comma position is {posComma}")
-        logging.info(f"The corrected county name is {county[:posComma]}")
-        county=county[:posComma]
+        logging.info(f"The corrected county name is "
+                     f"{county[:posComma]}")
+        county = county[:posComma]
 
     return county
 
+
+# ============================================ #
+# //            getTown Function            // #
+# ============================================ #
+
+
 def getTown(town: str):
+    """
+    Given a string of Town data, return the town name
+    """
     posCounty: int = town.find("Co.")
-    town = town[3:posCounty-1]
+    town = town[3:posCounty - 1]
     return town
+
+
+# -------------------------------------------- #
+# //               Main function            // #
+# -------------------------------------------- #
+
 
 def main():
     """This is the main() function"""
@@ -87,15 +97,18 @@ def main():
     logging.basicConfig(level=logging.INFO)
 
     # Pull the JSON data from the suppied url
-    with urllib.request.urlopen("https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/CD176/JSON-stat/1.0/en") as url:
+    with urllib.request.urlopen(
+        "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/CD176/JSON-stat/1.0/en"
+    ) as url:
         data = json.load(url)
         logging.info(data)
 
-    # Save the data to a json file in the current working directory called CSO.json
+    # Save the data to a json file in the current working directory
+    # called CSO.json
     outfile = open("CSO.json", "w")
     json.dump(data, outfile)
     outfile.close()
-    
+
     # Import CSO.json to a dictionary
     infile = open("CSO.json", "r")
     data = json.load(infile)
@@ -104,7 +117,9 @@ def main():
     # Build a lookup list from $.dataset.dimension.C03198V03862.category.label
     # This will allow us to look up the population data later
     lookup = {}
-    for key, value in data["dataset"]["dimension"]["C03198V03862"]["category"]["label"].items():
+    for key, value in data["dataset"]["dimension"]["C03198V03862"]["category"][
+        "label"
+    ].items():
         # Set the value to be the first 3 characters of the value item
         value = value[0:3]
         lookup[value] = key
@@ -132,7 +147,7 @@ def main():
     gTowns = {}
     logging.info("Building dictionary of G towns")
 
-    # step through the list of towns in allTowns. print each key and value in allTowns
+    # step through the list of towns in allTowns
     for key, town in allTowns.items():
         # print(f"Town: {town}")
         if town[4] == "G":
@@ -142,8 +157,10 @@ def main():
     logging.info("***Dictionary of G towns")
     logging.info(gTowns)
 
-    # So we have our list of G towns in a dictionary. We now need to step through the items in the dictionary, extract the index, use
-    # that index to lookup the data in the values list, and append it to CSV_Data
+    # So we have our list of G towns in a dictionary.
+    # We now need to step through the items in the dictionary,
+    # extract the index, use that index to lookup the data in
+    # the values list, and append it to CSV_Data
 
     CSV_Data = []
 
@@ -164,19 +181,37 @@ def main():
         # Get the pop_value for each town
         adjusted_key = (int(key) - 1) * 8
         pop_value = values[adjusted_key]
-        males_value = values[adjusted_key+1]
-        females_value = values[adjusted_key+2]
-        ph_occ_value = values[adjusted_key+3]
-        ph_unocc_value = values[adjusted_key+4]
-        vac_dwell_value = values[adjusted_key+5]
-        h_stock_value = values[adjusted_key+6]
-        vac_rate_value = values[adjusted_key+7]
+        males_value = values[adjusted_key + 1]
+        females_value = values[adjusted_key + 2]
+        ph_occ_value = values[adjusted_key + 3]
+        ph_unocc_value = values[adjusted_key + 4]
+        vac_dwell_value = values[adjusted_key + 5]
+        h_stock_value = values[adjusted_key + 6]
+        vac_rate_value = values[adjusted_key + 7]
 
         CSV_Data.append(
-            [key, town, pop_value, males_value, females_value, ph_occ_value, ph_unocc_value, vac_dwell_value, h_stock_value, vac_rate_value])
+            [
+                key,
+                town,
+                pop_value,
+                males_value,
+                females_value,
+                ph_occ_value,
+                ph_unocc_value,
+                vac_dwell_value,
+                h_stock_value,
+                vac_rate_value,
+            ]
+        )
 
-        #logging.info(f"Town: {town} -- Population: {pop_value}")
-        print(f"{key},{town},{pop_value},{males_value},{females_value},{ph_occ_value},{ph_unocc_value},{vac_dwell_value},{h_stock_value},{vac_rate_value}")
+        # logging.info(f"Town: {town} -- Population: {pop_value}")
+        print(
+            f"{key},{town},{pop_value},"
+            f"{males_value},{females_value},"
+            f"{ph_occ_value},{ph_unocc_value},"
+            f"{vac_dwell_value},{h_stock_value},"
+            f"{vac_rate_value}"
+        )
 
     # Open the letterG.csv file
     try:
@@ -189,10 +224,12 @@ def main():
         print(f"ERROR: New un-excepted error.h")
         raise OSError(e)
 
-    csv_writer = csv.writer(fh, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    csv_writer = csv.writer(fh, delimiter=",",
+                            quotechar='"',
+                            quoting=csv.QUOTE_MINIMAL)
     csv_writer.writerows(CSV_Data)
 
-    #print the type of data
+    # print the type of data
 
     # logging.info(type(data))
 
